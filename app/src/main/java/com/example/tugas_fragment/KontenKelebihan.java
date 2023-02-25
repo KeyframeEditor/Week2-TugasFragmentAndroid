23aa19887010c206505fd0a0bfc975f5353262a3
Andhika_R <andhika.rahmanu@mail.ugm.ac.id>
2023-02-26 00:54:37 +0700

    dinamis

--- app/src/main/java/com/example/tugas_fragment/KontenKelebihan.java
+++ app/src/main/java/com/example/tugas_fragment/KontenKelebihan.java
@@ -9,30 +9,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-public class konten extends Fragment {
+public class KontenKelebihan extends Fragment {
 
-    View view;
-
-    public konten() {
+    // TODO: RK
+    public KontenKelebihan() {
         // Required empty public constructor
     }
 
-    public static konten newInstance() {
-        return new konten();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_konten, container, false);
+        return inflater.inflate(R.layout.fragment_konten_kelebihan, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-    }
 
+    }
 }
--- app/src/main/java/com/example/tugas_fragment/MainActivity.java
+++ app/src/main/java/com/example/tugas_fragment/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.tugas_fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,17 +11,51 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonLihat;
+    private boolean kelebihanIsShown = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         buttonLihat = findViewById(R.id.buttonLihat);
+        displayDefault();
         buttonLihat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //button rules
+                if (!kelebihanIsShown){
+                    displayKelebihan();
+                }
+                else {
+                    closeKelebihan();
+                }
             }
         });
+    }
+
+    private void closeKelebihan() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new konten());
+        fragmentTransaction.commit();
+
+        buttonLihat.setText("Lihat Kelebihan");
+        kelebihanIsShown = false;
+    }
+
+    private void displayKelebihan() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new KontenKelebihan());
+        fragmentTransaction.commit();
+
+        buttonLihat.setText("Mantap!");
+        kelebihanIsShown = true;
+    }
+
+    private void displayDefault(){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, new konten());
+        fragmentTransaction.commit();
     }
 }
